32307e309928477d790cf2338b250db719fa35f7
codecracker0123 <131762670+codecracker0123@users.noreply.github.com>
2023-04-26 16:11:18 +0530

    Add files via upload

---
+++ 8-LP2.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+ int i,j,k;
+ clrscr();
+
+ for(i=1 ; i<=5 ; i++)
+ {
+     for(k=i ; k<=4 ; k++)
+     {
+	    printf("  ");
+     }
+     for(j=i ; j>=1 ; j--)
+     {
+	  printf("%d ",j);
+     }
+     printf("\n");
+
+ }
+ getch();
+}
